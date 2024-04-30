ab924108723ca54ab15627c942124aac02173be4
RochiFox <rochithefox@gmail.com>
2024-04-30 13:43:23 +0300

    Added: added theme switcher

--- client/src/pages/todoPage/TodoPage.jsx
+++ client/src/pages/todoPage/TodoPage.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import "./index.scss";
 
 export default function TodoPage() {
+  const [isDarkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div className="main">
-      <div className="main__background main__background_light" />
+    <div className={`main ${isDarkTheme ? "main_dark" : "main_light"}`}>
+      <div
+        className={`main__background ${
+          isDarkTheme ? "main__background_dark" : "main__background_light"
+        }`}
+      />
 
       <div className="main__block">
         <div className="main__section main__section_first">
           <h1 className="main__section-title">todo</h1>
 
-          <button className="main__theme-btn main__theme-btn_light"></button>
+          <button
+            className={`main__theme-btn ${
+              isDarkTheme ? "main__theme-btn_dark" : "main__theme-btn_light"
+            }`}
+            onClick={() => setDarkTheme(!isDarkTheme)}
+          />
         </div>
 
         <div className="main__section main__section_second"></div>
