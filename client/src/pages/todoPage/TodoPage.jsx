fd5cbfd6279a9ee956c7c0b29349195ea008f59e
RochiFox <rochithefox@gmail.com>
2024-05-03 12:45:57 +0300

    Added: added tabs section

--- client/src/pages/todoPage/TodoPage.jsx
+++ client/src/pages/todoPage/TodoPage.jsx
@@ -72,7 +72,143 @@ export default function TodoPage() {
           />
         </div>
 
-        <div className="main__section main__section_third"></div>
+        <div className="tabs-block">
+          <div
+            className={`todo-tab ${
+              isDarkTheme ? "todo-tab_dark" : "todo-tab_light"
+            }`}
+          >
+            <input
+              type="checkbox"
+              className={`todo-tab__check-btn ${
+                isDarkTheme
+                  ? "todo-tab__check-btn_dark"
+                  : "todo-tab__check-btn_light"
+              }`}
+            />
+
+            <p
+              className={`todo-tab__text ${
+                isDarkTheme ? "todo-tab__text_dark" : "todo-tab__text_light"
+              }`}
+            >
+              Testing IDK with very long message I think yeah... blah blahb
+              lha...
+            </p>
+          </div>
+
+          <div
+            className={`todo-tab ${
+              isDarkTheme ? "todo-tab_dark" : "todo-tab_light"
+            }`}
+          >
+            <input
+              type="checkbox"
+              className={`todo-tab__check-btn ${
+                isDarkTheme
+                  ? "todo-tab__check-btn_dark"
+                  : "todo-tab__check-btn_light"
+              }`}
+            />
+
+            <p
+              className={`todo-tab__text ${
+                isDarkTheme ? "todo-tab__text_dark" : "todo-tab__text_light"
+              }`}
+            >
+              Testing IDK with very long message I think yeah... blah blahb
+              lha...
+            </p>
+          </div>
+
+          <div
+            className={`todo-tab ${
+              isDarkTheme ? "todo-tab_dark" : "todo-tab_light"
+            }`}
+          >
+            <input
+              type="checkbox"
+              className={`todo-tab__check-btn ${
+                isDarkTheme
+                  ? "todo-tab__check-btn_dark"
+                  : "todo-tab__check-btn_light"
+              }`}
+            />
+
+            <p
+              className={`todo-tab__text ${
+                isDarkTheme ? "todo-tab__text_dark" : "todo-tab__text_light"
+              }`}
+            >
+              Testing IDK with very long message I think yeah... blah blahb
+              lha...
+            </p>
+          </div>
+
+          <div
+            className={`total-lists ${
+              isDarkTheme ? "total-lists_dark" : "total-lists_light"
+            }`}
+          >
+            <p
+              className={`total-lists__count ${
+                isDarkTheme
+                  ? "total-lists__count_dark"
+                  : "total-lists__count-light"
+              }`}
+            >
+              <span className="count">3</span> items left
+            </p>
+
+            <div className="total-lists__filter-block">
+              <button
+                className={`total-lists__btn ${
+                  isDarkTheme
+                    ? "total-lists__btn_dark"
+                    : "total-lists__btn_light"
+                }`}
+              >
+                All
+              </button>
+
+              <button
+                className={`total-lists__btn ${
+                  isDarkTheme
+                    ? "total-lists__btn_dark"
+                    : "total-lists__btn_light"
+                }`}
+              >
+                Active
+              </button>
+
+              <button
+                className={`total-lists__btn ${
+                  isDarkTheme
+                    ? "total-lists__btn_dark"
+                    : "total-lists__btn_light"
+                }`}
+              >
+                Completed
+              </button>
+            </div>
+
+            <button
+              className={`total-lists__btn ${
+                isDarkTheme ? "total-lists__btn_dark" : "total-lists__btn_light"
+              }`}
+            >
+              Clear Completed
+            </button>
+          </div>
+        </div>
+
+        <p
+          className={`main__subtitle ${
+            isDarkTheme ? "main__subtitle_dark" : "main__subtitle_light"
+          }`}
+        >
+          Drag and drop to reorder list
+        </p>
       </div>
     </div>
   );
