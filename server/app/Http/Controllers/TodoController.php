f3766d8c792caa44faa703e5780f292a53bcfb42
RochiFox <rochithefox@gmail.com>
2024-05-07 14:33:09 +0300

    Added: added todo functions into controller

--- server/app/Http/Controllers/TodoController.php
+++ server/app/Http/Controllers/TodoController.php
@@ -3,10 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    public function index() {
-        
+    public function index()
+    {
+        $todos = Todo::all();
+
+        return response()->json($todos);
+    }
+
+    public function create(Request $request)
+    {
+        $todo = Todo::create([
+            'description' => $request->description,
+            'completed' => false,
+        ]);
+
+        return response()->json($todo);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::find($id);
+
+        $todo->completed = $request->has('completed') ? $request->completed : $todo->completed;
+        $todo->save();
+
+        return response()->json($todo);
+    }
+
+    public function delete()
+    {
+        $todo = Todo::where('completed', true)->delete();
+
+        return response()->json($todo);
     }
 }
--- server/database/migrations/2024_05_06_111426_create_todo_table.php
+++ server/database/migrations/2024_05_06_111426_create_todo_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('todo', function (Blueprint $table) {
             $table->id();
-            $table->text('description');
+            $table->string('description');
             $table->boolean('completed');
             $table->timestamps();
         });
